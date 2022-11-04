e0cbb55d2fc24bc989e7bdde625f75f437349a42
dkhenrique <henrihk6@gmail.com>
2022-11-04 10:53:41 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import  COLORS  from './src/styles/theme';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={COLORS}>
+      
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
